6b9e8d6f975b6329475733d820a74fe18e43e253
avoidik <avoidik@gmail.com>
2018-04-16 16:51:37 +0300

    Allow init & unseal in one go with additional parameters

--- cmd/bank-vaults/unseal.go
+++ cmd/bank-vaults/unseal.go
@@ -10,9 +10,11 @@ import (
 )
 
 const cfgUnsealPeriod = "unseal-period"
+const cfgInit = "init"
 
 type unsealCfg struct {
 	unsealPeriod time.Duration
+	proceedInit  bool
 }
 
 var unsealConfig unsealCfg
@@ -28,7 +30,10 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		appConfig.BindPFlag(cfgUnsealPeriod, cmd.PersistentFlags().Lookup(cfgUnsealPeriod))
+		appConfig.BindPFlag(cfgInitRootToken, cmd.PersistentFlags().Lookup(cfgInitRootToken))
+		appConfig.BindPFlag(cfgStoreRootToken, cmd.PersistentFlags().Lookup(cfgStoreRootToken))
 		unsealConfig.unsealPeriod = appConfig.GetDuration(cfgUnsealPeriod)
+		unsealConfig.proceedInit = appConfig.GetBool(cfgInit)
 
 		store, err := kvStoreForConfig(appConfig)
 
@@ -56,6 +61,14 @@ to quickly create a Cobra application.`,
 
 		for {
 			func() {
+				if unsealConfig.proceedInit {
+					if err = v.Init(); err != nil {
+						logrus.Fatalf("error initialising vault: %s", err.Error())
+					} else {
+						unsealConfig.proceedInit = false
+					}
+				}
+
 				logrus.Infof("checking if vault is sealed...")
 				sealed, err := v.Sealed()
 				if err != nil {
@@ -85,6 +98,9 @@ to quickly create a Cobra application.`,
 
 func init() {
 	unsealCmd.PersistentFlags().Duration(cfgUnsealPeriod, time.Second*30, "How often to attempt to unseal the vault instance")
+	unsealCmd.PersistentFlags().Bool(cfgInit, false, "Initialize vault instantce if not yet initialized")
+	unsealCmd.PersistentFlags().String(cfgInitRootToken, "", "root token for the new vault cluster (only if -init=true)")
+	unsealCmd.PersistentFlags().Bool(cfgStoreRootToken, true, "should the root token be stored in the key store (only if -init=true)")
 
 	rootCmd.AddCommand(unsealCmd)
 }
