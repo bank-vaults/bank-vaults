12e0c31655a102cd4f9f51b2e6f4a08a4c9c49d8
Cosmin Cojocar <cosmin.cojocar@gmx.ch>
2018-07-11 13:19:50 +0200

    Configure the operator namespace over an environment variable

--- operator/cmd/vault-operator/main.go
+++ operator/cmd/vault-operator/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"os"
 	"runtime"
 
 	stub "github.com/banzaicloud/bank-vaults/operator/pkg/stub"
@@ -11,15 +12,19 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-func printVersion() {
+const operatorNamespace = "OPERATOR_NAMESPACE"
+
+func printVersion(namespace string) {
 	logrus.Infof("Go Version: %s", runtime.Version())
 	logrus.Infof("Go OS/Arch: %s/%s", runtime.GOOS, runtime.GOARCH)
 	logrus.Infof("operator-sdk Version: %v", sdkVersion.Version)
+	logrus.Infof("operator namespace: %s", namespace)
 }
 
 func main() {
-	printVersion()
-	sdk.Watch("vault.banzaicloud.com/v1alpha1", "Vault", "", 5)
+	ns := os.Getenv(operatorNamespace)
+	printVersion(ns)
+	sdk.Watch("vault.banzaicloud.com/v1alpha1", "Vault", ns, 5)
 	sdk.Handle(stub.NewHandler())
 	sdk.Run(context.TODO())
 }
