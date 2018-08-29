cddb23343569fd8577f38e6755e1fbe97b71fb14
Nandor Kracser <bonifaido@gmail.com>
2018-08-29 14:53:01 +0200

    Use client-go 7.0.0 to remove juju GPL code

--- vendor/k8s.io/code-generator/_examples/crd/informers/externalversions/example/v1/interface.go
+++ vendor/k8s.io/code-generator/_examples/crd/informers/externalversions/example/v1/interface.go
@@ -1,5 +1,5 @@
 /*
-Copyright The Kubernetes Authors.
+Copyright 2018 The Kubernetes Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -24,8 +24,6 @@ import (
 
 // Interface provides access to all the informers in this group version.
 type Interface interface {
-	// ClusterTestTypes returns a ClusterTestTypeInformer.
-	ClusterTestTypes() ClusterTestTypeInformer
 	// TestTypes returns a TestTypeInformer.
 	TestTypes() TestTypeInformer
 }
@@ -39,11 +37,6 @@ type version struct {
 // New returns a new Interface.
 func New(f internalinterfaces.SharedInformerFactory, namespace string, tweakListOptions internalinterfaces.TweakListOptionsFunc) Interface {
 	return &version{factory: f, namespace: namespace, tweakListOptions: tweakListOptions}
-}
-
-// ClusterTestTypes returns a ClusterTestTypeInformer.
-func (v *version) ClusterTestTypes() ClusterTestTypeInformer {
-	return &clusterTestTypeInformer{factory: v.factory, tweakListOptions: v.tweakListOptions}
 }
 
 // TestTypes returns a TestTypeInformer.
