990dd0fefac4fb9e67ff53d4edf7ae3c48de5fe9
Nandor Kracser <bonifaido@gmail.com>
2018-12-12 18:02:43 -0800

    Improve Azure KV authentication with NewAuthorizerFromEnvironment

--- pkg/kv/azurekv/iam.go
+++ pkg/kv/azurekv/iam.go
@@ -15,78 +15,34 @@
 package azurekv
 
 import (
-	"encoding/json"
-	"io/ioutil"
 	"log"
-	"net/url"
 	"os"
 	"strings"
 
 	"github.com/Azure/go-autorest/autorest"
-	"github.com/Azure/go-autorest/autorest/adal"
 	"github.com/Azure/go-autorest/autorest/azure"
+	"github.com/Azure/go-autorest/autorest/azure/auth"
 )
-
-type credentials struct {
-	ClientID       string `json:"clientId,omitempty"`
-	ClientSecret   string `json:"clientSecret,omitempty"`
-	SubscriptionID string `json:"subscriptionId,omitempty"`
-	TenantID       string `json:"tenantId,omitempty"`
-}
 
 var (
 	// for service principal and device
 	keyvaultAuthorizer autorest.Authorizer
-	creds              credentials
 )
 
 func init() {
+	var err error
+	resource := strings.TrimSuffix(azure.PublicCloud.KeyVaultEndpoint, "/")
 	if _, ok := os.LookupEnv("AZURE_AUTH_LOCATION"); ok {
-
-		fileLocation := os.Getenv("AZURE_AUTH_LOCATION")
-		if fileLocation == "" {
-			log.Fatal("auth file not found. Environment variable AZURE_AUTH_LOCATION is not set")
-		}
-
-		contents, err := ioutil.ReadFile(fileLocation)
-		if err != nil {
-			log.Fatal(err.Error())
-		}
-
-		err = json.Unmarshal(contents, &creds)
-		if err != nil {
-			log.Fatal(err.Error())
-		}
+		keyvaultAuthorizer, err = auth.NewAuthorizerFromFileWithResource(resource)
 	} else {
-		// https://github.com/Azure/azure-sdk-for-node/issues/1932#issuecomment-253946276
-		creds.TenantID = os.Getenv("AZURE_TENANT_ID")
-		creds.ClientID = os.Getenv("AZURE_CLIENT_ID")
-		creds.ClientSecret = os.Getenv("AZURE_CLIENT_SECRET")
+		keyvaultAuthorizer, err = auth.NewAuthorizerFromEnvironmentWithResource(resource)
+	}
+	if err != nil {
+		log.Fatal(err)
 	}
 }
 
 // GetKeyvaultAuthorizer gets an authorizer for the keyvault dataplane
-func GetKeyvaultAuthorizer() (a autorest.Authorizer, err error) {
-	if keyvaultAuthorizer != nil {
-		return keyvaultAuthorizer, nil
-	}
-
-	config, err := adal.NewOAuthConfig(azure.PublicCloud.ActiveDirectoryEndpoint, creds.TenantID)
-	updatedAuthorizeEndpoint, err := url.Parse("https://login.windows.net/" + creds.TenantID + "/oauth2/token")
-	config.AuthorizeEndpoint = *updatedAuthorizeEndpoint
-	if err != nil {
-		return
-	}
-
-	token, err := adal.NewServicePrincipalToken(*config, creds.ClientID, creds.ClientSecret, strings.TrimSuffix(azure.PublicCloud.KeyVaultEndpoint, "/"))
-	if err != nil {
-		return a, err
-	}
-	a = autorest.NewBearerAuthorizer(token)
-
-	if err == nil {
-		keyvaultAuthorizer = a
-	}
-
-	return
+func GetKeyvaultAuthorizer() autorest.Authorizer {
+	return keyvaultAuthorizer
 }
