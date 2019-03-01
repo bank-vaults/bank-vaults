c857a41071709d3d3e96a07b68a580270ed3e064
Denis Baryshev <dennybaa@gmail.com>
2019-03-01 14:15:25 +0300

    [vault-env] added VAULT_* environment sanitization
    Signed-off-by: Denis Baryshev <dennybaa@gmail.com>

--- cmd/vault-env/main.go
+++ cmd/vault-env/main.go
@@ -26,6 +26,39 @@ import (
 	"github.com/spf13/cast"
 )
 
+type sanitizedEnviron []string
+
+var sanitizeEnvmap = map[string]bool {
+	"VAULT_TOKEN": true,
+	"VAULT_ADDR": true,
+	"VAULT_CACERT": true,
+	"VAULT_CAPATH": true,
+	"VAULT_CLIENT_CERT": true,
+	"VAULT_CLIENT_KEY": true,
+	"VAULT_CLIENT_TIMEOUT": true,
+	"VAULT_CLUSTER_ADDR": true,
+	"VAULT_MAX_RETRIES": true,
+	"VAULT_REDIRECT_ADDR": true,
+	"VAULT_SKIP_VERIFY": true,
+	"VAULT_TLS_SERVER_NAME": true,
+	"VAULT_CLI_NO_COLOR": true,
+	"VAULT_RATE_LIMIT": true,
+	"VAULT_NAMESPACE": true,
+	"VAULT_MFA": true,
+	"VAULT_ROLE": true,
+	"VAULT_PATH": true,
+}
+
+// Appends variable an entry (name=value) into the environ list.
+// VAULT_* variables are not populated into this list.
+func (environ *sanitizedEnviron) append(iname interface{}, ivalue interface{}) {
+	name, value := iname.(string), ivalue.(string)
+	if _, ok := sanitizeEnvmap[name]; !ok {
+		*environ = append(*environ, fmt.Sprintf("%s=%s", name, value))
+	}
+}
+
+
 func main() {
 
 	role := os.Getenv("VAULT_ROLE")
@@ -43,14 +76,16 @@ func main() {
 		os.Exit(1)
 	}
 
+	// initial and sanitized environs
 	environ := syscall.Environ()
+	sanitized := make(sanitizedEnviron, 0, len(environ))
 
-	for i, env := range environ {
+	for _, env := range environ {
 		split := strings.SplitN(env, "=", 2)
 		name := split[0]
 		value := split[1]
-		if strings.HasPrefix(value, "vault:") {
 
+		if strings.HasPrefix(value, "vault:") {
 			path := strings.TrimPrefix(value, "vault:")
 			split := strings.SplitN(path, "#", 2)
 			path = split[0]
@@ -78,12 +113,14 @@ func main() {
 					data = cast.ToStringMap(secret.Data)
 				}
 				if value, ok := data[key]; ok {
-					environ[i] = fmt.Sprintf("%s=%s", name, value)
+					sanitized.append(name, value)
 				} else {
 					fmt.Fprintf(os.Stderr, "key not found: %s\n", key)
 					os.Exit(1)
 				}
 			}
+		} else {
+			sanitized.append(name, value)
 		}
 	}
 
@@ -96,7 +133,7 @@ func main() {
 			fmt.Fprintf(os.Stderr, "binary not found: %s\n", os.Args[1])
 			os.Exit(1)
 		}
-		err = syscall.Exec(binary, os.Args[1:], environ)
+		err = syscall.Exec(binary, os.Args[1:], sanitized)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "failed to exec process '%s': %s\n", binary, err.Error())
 			os.Exit(1)
