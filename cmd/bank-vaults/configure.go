e94bcc8fe2e6420902ad1928b42fe4577d4a198d
Francesco Ciocchetti <primeroznl@gmail.com>
2019-04-15 23:05:53 +0200

    Re-inject configuration files into the configurations channel on errors
    * Achieve an eventually consistent configuration
    * backoff up to 30s of sleep to avoid looping over a syntax error

--- cmd/bank-vaults/configure.go
+++ cmd/bank-vaults/configure.go
@@ -28,6 +28,7 @@ import (
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"github.com/jpillora/backoff"
 )
 
 const (
@@ -94,9 +95,18 @@ var configureCmd = &cobra.Command{
 			close(configurations)
 		}
 
+
+		// Handle backoff for configuration errors
+		b := &backoff.Backoff{
+			Min:    500 * time.Millisecond,
+			Max:    30 * time.Second,
+			Factor: 2,
+			Jitter: false,
+		}
+
 		for config := range configurations {
 
-			logrus.Infoln("config file has changed:", config.ConfigFileUsed())
+			logrus.Infoln("applying config file :", config.ConfigFileUsed())
 
 			func() {
 				for {
@@ -123,9 +133,11 @@ var configureCmd = &cobra.Command{
 							os.Exit(1)
 						}
 						failedConfigurationsCount++
+						go handleConfigurationError(config.ConfigFileUsed(), configurations, b.Duration())
 						return
 					}
 
+					b.Reset()
 					successfulConfigurationsCount++
 					logrus.Info("successfully configured vault")
 					return
@@ -133,6 +145,12 @@ var configureCmd = &cobra.Command{
 			}()
 		}
 	},
+}
+
+func handleConfigurationError(vaultConfigFile string, configurations chan *viper.Viper, sleepTime time.Duration) {
+	logrus.Infof("Failed applying configuration file: %s , sleeping for %s before trying again", vaultConfigFile, sleepTime)
+	time.Sleep(sleepTime)
+	configurations <- parseConfiguration(vaultConfigFile)
 }
 
 func watchConfigurations(vaultConfigFiles []string, configurations chan *viper.Viper) {
