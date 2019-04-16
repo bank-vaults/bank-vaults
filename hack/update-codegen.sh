9a1ee5c7019cb5563d676cee10f33c0ccdfd35e9
Nandor Kracser <bonifaido@gmail.com>
2019-04-16 14:15:20 +0200

    operator: regenerate client

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -18,8 +18,17 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+function finish {
+  rm -rf ${CODEGEN_DIR}
+}
+
+trap finish EXIT
+
+CODEGEN_DIR=$(mktemp -d)
+
+git clone --depth 1 git@github.com:kubernetes/code-generator.git ${CODEGEN_DIR}
+
 SCRIPT_ROOT=$(dirname ${BASH_SOURCE})/..
-CODEGEN_PKG=${CODEGEN_PKG:-$(cd ${SCRIPT_ROOT}; ls -d -1 ./vendor/k8s.io/code-generator 2>/dev/null || echo ../code-generator)}
 
 OUTDIR=$(dirname ${BASH_SOURCE})/../../../..
 
@@ -30,7 +39,7 @@ echo "Generating code to ${OUTDIR}"
 # --output-base    because this script should also be able to run inside the vendor dir of
 #                  k8s.io/kubernetes. The output-base is needed for the generators to output into the vendor dir
 #                  instead of the $GOPATH directly. For normal projects this can be dropped.
-${CODEGEN_PKG}/generate-groups.sh all \
+${CODEGEN_DIR}/generate-groups.sh all \
   github.com/banzaicloud/bank-vaults/operator/pkg/client github.com/banzaicloud/bank-vaults/operator/pkg/apis \
   vault:v1alpha1 \
   --output-base "${OUTDIR}" \
