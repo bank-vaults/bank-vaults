f43fea4b89f8c31c644e60f12fa006e858a6af3b
Peter Balogh <p.balogh.sa@gmail.com>
2019-06-01 13:19:35 +0200

    initial serret mutator

---
+++ cmd/vault-secrets-webhook/secret.go
@@ -0,0 +1,166 @@
+// Copyright © 2019 Banzai Cloud
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/banzaicloud/bank-vaults/pkg/vault"
+	vaultapi "github.com/hashicorp/vault/api"
+	"github.com/slok/kubewebhook/pkg/log"
+	"github.com/spf13/cast"
+	"github.com/spf13/viper"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+type dockerCreds struct {
+	Auths map[string]*dockerCred `json:"auths"`
+}
+
+type dockerCred struct {
+	Username string `json:"username,omitempty"`
+	Password string `json:"password,omitempty"`
+	Auth     []byte `json:"auth"`
+}
+
+func mutateSecret(obj metav1.Object, secret *corev1.Secret, vaultConfig vaultConfig, ns string) error {
+	logger := &log.Std{Debug: viper.GetBool("debug")}
+	logger.Debugf("SecretData: %s", secret.Data)
+
+	for key, value := range secret.Data {
+		if key == ".dockerconfigjson" {
+			var dc dockerCreds
+			_ = json.Unmarshal(value, &dc)
+			mutateDockerCreds(secret, &dc, vaultConfig)
+		} else {
+			sc := map[string]string{
+				key: string(value),
+			}
+			mutated, _ := mutateSecretCreds(secret, sc, vaultConfig)
+			if mutated {
+				logger.Infof("Mutated successfully")
+			}
+		}
+	}
+
+	return nil
+}
+
+func mutateDockerCreds(secret *corev1.Secret, dc *dockerCreds, vaultConfig vaultConfig) (bool, error) {
+	mutated := false
+	logger := &log.Std{Debug: viper.GetBool("debug")}
+
+	
+	for key, creds := range dc.Auths {
+		if strings.HasPrefix(string(creds.Auth), "vault:") {
+			logger.Debugf("auth %s %s", key, creds.Auth)
+			split := strings.Split(string(creds.Auth), ":")
+			username := fmt.Sprintf("%s:%s", split[0], split[1])
+			password := fmt.Sprintf("%s:%s", split[2], split[3])
+
+			credPath := map[string]string{
+				"username": username,
+				"password": password,
+			}
+
+			dcCreds := getCredsFromVault(credPath, vaultConfig)
+			logger.Infof("dccreds %s --- %s", key, dcCreds)
+
+			dockerAuths := dockerCred{
+				Auth: []byte(fmt.Sprintf("%s:%s", dcCreds["username"], dcCreds["password"])),
+			}
+			if creds.Username != "" && creds.Password != "" {
+				dockerAuths.Username = dcCreds["password"]
+				dockerAuths.Password = dcCreds["password"]
+			}
+		}
+	}
+	//logger.Infof("assembled %v", dockerCred)
+
+	return mutated, nil
+}
+
+func mutateSecretCreds(secret *corev1.Secret, sc map[string]string, vaultConfig vaultConfig) (bool, error) {
+	mutated := false
+
+	logger := &log.Std{Debug: viper.GetBool("debug")}
+	logger.Infof("simple secret %s", sc)
+
+	secCreds := getCredsFromVault(sc, vaultConfig)
+	for key, value := range secCreds {
+		secret.Data[key] = []byte(value)
+		mutated = true
+	}
+	return mutated, nil
+}
+
+func getCredsFromVault(creds map[string]string, vaultConfig vaultConfig) map[string]string {
+	logger := &log.Std{Debug: viper.GetBool("debug")}
+	os.Setenv("VAULT_ADDR", vaultConfig.addr)
+	os.Setenv("VAULT_SKIP_VERIFY", vaultConfig.skipVerify)
+
+	logger.Debugf("Vaultconfig %s", vaultConfig)
+
+	client, err := vault.NewClientWithOptions(
+		vault.ClientRole(vaultConfig.role),
+		vault.ClientAuthPath(vaultConfig.path),
+	)
+
+	if err != nil {
+		logger.Errorf("Failed to create vault client")
+	}
+
+	var secCreds = make(map[string]string)
+	for key, value := range creds {
+		if strings.HasPrefix(value, "vault:") {
+			path := strings.TrimPrefix(value, "vault:")
+			split := strings.SplitN(path, "#", 3)
+			path = split[0]
+			var vaultKey string
+			if len(split) > 1 {
+				vaultKey = split[1]
+			}
+			version := "-1"
+			if len(split) == 3 {
+				version = split[2]
+			}
+
+			var secret *vaultapi.Secret
+			var err error
+			var data map[string]interface{}
+
+			secret, err = client.Vault().Logical().ReadWithData(path, map[string][]string{"version": {version}})
+			if err != nil {
+				logger.Errorf("Failed to read secret path: %s error: %s", path, err.Error())
+			}
+			if secret == nil {
+				logger.Errorf("Path not found path: %s", path)
+			} else {
+				v2Data, ok := secret.Data["data"]
+				if ok {
+					data = cast.ToStringMap(v2Data)
+				} else {
+					data = cast.ToStringMap(secret.Data)
+				}
+			}
+			secCreds[key] = cast.ToString(data[vaultKey])
+		}
+	}
+	return secCreds
+}
