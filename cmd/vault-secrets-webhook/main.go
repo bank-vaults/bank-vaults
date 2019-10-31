c0fce7e3ae9ef3cac4d23b8dcd9e2ce45a0cfe08
l.aminov <l.aminov@tinkoff.ru>
2019-10-31 16:04:14 +0500

    Remove opts_ part from variable

--- cmd/vault-secrets-webhook/main.go
+++ cmd/vault-secrets-webhook/main.go
@@ -916,7 +916,7 @@ func init() {
 	viper.SetDefault("debug", "false")
 	viper.SetDefault("enable_json_log", "false")
 	viper.SetDefault("registry_cache_storage", "inmemory")
-	viper.SetDefault("registry_cache_opts_digest_only", "false")
+	viper.SetDefault("registry_cache_digest_only", "false")
 	viper.AutomaticEnv()
 
 	logger = log.New()
