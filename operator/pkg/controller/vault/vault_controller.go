e40b8a5a1cc79fc47c07d46af24153dbcd201898
Nandor Kracser <bonifaido@gmail.com>
2019-11-29 13:29:01 +0100

    simplify and align patch log line
    Signed-off-by: Nandor Kracser <bonifaido@gmail.com>

--- operator/pkg/controller/vault/vault_controller.go
+++ operator/pkg/controller/vault/vault_controller.go
@@ -169,11 +169,11 @@ func createOrUpdateObjectWithClient(c client.Client, o runtime.Object) error {
 		}
 
 		if !result.IsEmpty() {
-			log.V(1).Info("resource diffs",
+			log.V(1).Info(fmt.Sprintf("Resource update for object %s:%s", o.GetObjectKind(), o.(metav1.ObjectMetaAccessor).GetObjectMeta().GetName()),
 				"patch", string(result.Patch),
-				"original", string(result.Original),
-				"modified", string(result.Modified),
-				"current", string(result.Current),
+				// "original", string(result.Original),
+				// "modified", string(result.Modified),
+				// "current", string(result.Current),
 			)
 
 			err := patch.DefaultAnnotator.SetLastAppliedAnnotation(o)
