5e66bf8910520df00b76fca08b14921c8bb445f4
Nandor Kracser <bonifaido@gmail.com>
2019-12-07 17:28:52 +0100

    vault/chart: put under acceptance testing
    Signed-off-by: Nandor Kracser <bonifaido@gmail.com>

--- hack/acceptance-test.sh
+++ hack/acceptance-test.sh
@@ -37,6 +37,11 @@ trap finish EXIT
 # Create a resource quota in the default namespace
 kubectl create quota bank-vaults --hard=cpu=2,memory=4G,pods=10,services=10,replicationcontrollers=10,secrets=10,persistentvolumeclaims=10
 
+# Smoke test the pure Vault Helm chart first
+helm install --name vault --wait ./charts/vault
+helm delete --purge vault
+kubectl delete secret bank-vaults
+
 # Install the operators and companion
 kubectl apply -f operator/deploy/etcd-rbac.yaml
 kubectl apply -f operator/deploy/etcd-operator.yaml
