bf763a153425217ff1aa68bbf746a1e3aab45337
Nandor Kracser <bonifaido@gmail.com>
2020-01-22 11:59:31 +0100

    templates: get rid of dockerize
    Signed-off-by: Nandor Kracser <bonifaido@gmail.com>

--- cmd/template/main.go
+++ cmd/template/main.go
@@ -18,28 +18,82 @@ import (
 	"flag"
 	"io/ioutil"
 	"os"
+	"strings"
 
 	"github.com/banzaicloud/bank-vaults/internal/configuration"
-
 	log "github.com/sirupsen/logrus"
 )
+
+type arrayFlags []string
+
+func (i *arrayFlags) String() string {
+	return "my string representation"
+}
+
+func (i *arrayFlags) Set(value string) error {
+	*i = append(*i, value)
+	return nil
+}
 
 // template is an internal CLI command and not supported for direct consumption.
 func main() {
 
-	filename := flag.String("file", "/vault/config/vault.json", "location of the templated config file")
+	var filename string
+	var delimiters string
+	var templates arrayFlags
+
+	flag.StringVar(&delimiters, "delims", "${:}", "delimiters delimited by :")
+	flag.StringVar(&filename, "file", "/vault/config/vault.json", "the destination file templated from VAULT_LOCAL_CONFIG")
+	flag.Var(&templates, "template", "template filename pairs delimited by :")
 
 	flag.Parse()
 
-	vaultConfig := os.Getenv("VAULT_LOCAL_CONFIG")
-
-	buffer, err := configuration.EnvTemplate(vaultConfig)
-	if err != nil {
-		log.Fatalf("error executing config template: %s", err.Error())
+	delimitersArray := strings.Split(delimiters, ":")
+	if len(delimitersArray) != 2 {
+		log.Fatal("delims must be two mnemonics delimited by a :")
 	}
 
-	err = ioutil.WriteFile(*filename, buffer.Bytes(), 0600)
-	if err != nil {
-		log.Fatalf("error writing config file: %s", err.Error())
+	leftDelimiter := delimitersArray[0]
+	rightDelimiter := delimitersArray[1]
+
+	templater := configuration.NewTemplater(leftDelimiter, rightDelimiter)
+
+	vaultConfig := os.Getenv("VAULT_LOCAL_CONFIG")
+
+	if vaultConfig != "" {
+		buffer, err := templater.EnvTemplate(vaultConfig)
+		if err != nil {
+			log.Fatalf("error executing template: %s", err.Error())
+		}
+
+		err = ioutil.WriteFile(filename, buffer.Bytes(), 0600)
+		if err != nil {
+			log.Fatalf("error writing template file: %s", err.Error())
+		}
+	} else {
+		for _, t := range templates {
+			templateArray := strings.Split(t, ":")
+			if len(templateArray) != 2 {
+				log.Fatal("template must be two filenames delimited by a :")
+			}
+
+			source := templateArray[0]
+			destination := templateArray[1]
+
+			templateText, err := ioutil.ReadFile(source)
+			if err != nil {
+				log.Fatalf("error reading template file: %s", err.Error())
+			}
+
+			templatedText, err := templater.EnvTemplate(string(templateText))
+			if err != nil {
+				log.Fatalf("error executing template: %s", err.Error())
+			}
+
+			err = ioutil.WriteFile(destination, templatedText.Bytes(), 0600)
+			if err != nil {
+				log.Fatalf("error writing template file %q: %s", destination, err.Error())
+			}
+		}
 	}
 }
