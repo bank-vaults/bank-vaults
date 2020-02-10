225dd086d7c2189e2401879a9409ed5b585e2826
Salvatore Mazzarino <dev@mazzarino.cz>
2020-02-10 15:56:42 +0100

    Make unseal cmd platform-agnostic
    Signed-off-by: Salvatore Mazzarino <dev@mazzarino.cz>

--- pkg/sdk/vault/operator_client.go
+++ pkg/sdk/vault/operator_client.go
@@ -84,6 +84,7 @@ var _ Vault = &vault{}
 // a Vault server.
 type Vault interface {
 	Init() error
+	isInit() (bool, error)
 	RaftJoin(string) error
 	Sealed() (bool, error)
 	Active() (bool, error)
@@ -351,6 +352,20 @@ func (v *vault) Init() error {
 	}
 
 	return nil
+}
+
+// in our case Vault is initialized when root key is stored in the Cloud KMS
+func (v *vault) isInit() (bool, error) {
+	rootToken, err := v.keyStore.Get(v.rootTokenKey())
+	if err != nil {
+		return fmt.Errorf("unable to get key '%s': %s", v.rootTokenKey(), err.Error())
+	}
+
+	if len(rootToken) > 0 {
+		return true, nil
+	}
+
+	return false, nil
 }
 
 // RaftJoin joins Vault raft cluster if is not initialized already
