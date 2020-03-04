e42e1c67ab61cc95407e14edfa5c86565542e342
Salvatore Mazzarino <dev@mazzarino.cz>
2020-03-04 11:36:32 +0100

    debug - wip
    Signed-off-by: Salvatore Mazzarino <dev@mazzarino.cz>

--- hack/acceptance-test.sh
+++ hack/acceptance-test.sh
@@ -14,6 +14,8 @@ function finish {
     kubectl get pods
     kubectl describe pods
     kubectl logs deployment/vault-operator
+    kubectl get secret -n default
+    kubectl get secret -n default -oyaml vault-root
     echo "pod/vault-1 logs"
     kubectl logs --all-containers pod/vault-1
     kubectl logs --all-containers statefulset/vault
