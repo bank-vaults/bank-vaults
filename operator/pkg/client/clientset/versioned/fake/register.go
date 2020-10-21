3197156a1fe3719a250fbfd249d37dc177f0c899
Nándor István Krácser <bonifaido@gmail.com>
2020-10-21 14:12:01 +0200

    update slok/kubewebhook and code-generator (#1161)
    Signed-off-by: Nandor Kracser <bonifaido@gmail.com>

--- operator/pkg/client/clientset/versioned/fake/register.go
+++ operator/pkg/client/clientset/versioned/fake/register.go
@@ -27,7 +27,7 @@ import (
 
 var scheme = runtime.NewScheme()
 var codecs = serializer.NewCodecFactory(scheme)
-var parameterCodec = runtime.NewParameterCodec(scheme)
+
 var localSchemeBuilder = runtime.SchemeBuilder{
 	vaultv1alpha1.AddToScheme,
 }
