bb19a0799fb6a493a31ed3f18532ebb3b08efe49
Islam Wazery <wazery@ubuntu.com>
2022-02-03 10:42:19 +0100

    Use correct year in secret engine file

--- internal/vault/secrets_engines.go
+++ internal/vault/secrets_engines.go
@@ -1,4 +1,4 @@
-// Copyright © 2021 Banzai Cloud
+// Copyright © 2022 Banzai Cloud
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
