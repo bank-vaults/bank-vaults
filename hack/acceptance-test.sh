ed3086e4d4a4cc3801810cc86ba8ecb59dbe7feb
Ernad Halilovic <ernadh@gmail.com>
2022-02-23 15:54:35 +0100

    wip

--- hack/acceptance-test.sh
+++ hack/acceptance-test.sh
@@ -148,7 +148,7 @@ kubectl apply -f deploy/test-deploy-templating.yaml
 kubectl get pods -A
 kubectl wait pod -l app.kubernetes.io/name=test-templating --for=condition=ready --timeout=120s -A
 kubectl get pods -A
-test $(kubectl exec -it $(kubectl get pods --selector=app.kubernetes.io/name=test-templating -o=jsonpath='{.items[0].metadata.name}') -c alpine -- cat /vault/secrets/config.yaml | jq '.id' | xargs ) = "secretId"
+test $(kubectl -n vswh exec -it $(kubectl get pods -n vswh --selector=app.kubernetes.io/name=test-templating -o=jsonpath='{.items[0].metadata.name}') -c alpine -- cat /vault/secrets/config.yaml | jq '.id' | xargs ) = "secretId"
 
 kubectl apply -f deploy/test-deployment-seccontext.yaml
 kubectl wait --for=condition=available deployment/hello-secrets-seccontext --timeout=120s
