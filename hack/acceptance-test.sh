c3c15b3ce7420e298e2776c048ac640a8bfe86aa
Ernad Halilovic <ernadh@gmail.com>
2022-02-25 11:20:35 +0100

    wip

--- hack/acceptance-test.sh
+++ hack/acceptance-test.sh
@@ -147,13 +147,13 @@ test "$(kubectl get cm sample-configmap -o jsonpath='{.data.aws-access-key-id-in
 kubectl apply -f deploy/test-deploy-templating.yaml
 kubectl get pods -A
 sleep 10
-kubectl -n default logs $(kubectl get pods -n default --selector=app.kubernetes.io/name=test-templating -o=jsonpath='{.items[0].metadata.name}') -c vault-agent
-kubectl -n default logs $(kubectl get pods -n default --selector=app.kubernetes.io/name=test-templating -o=jsonpath='{.items[0].metadata.name}') -c alpine
+kubectl -n vswh logs $(kubectl get pods -n vswh --selector=app.kubernetes.io/name=test-templating -o=jsonpath='{.items[0].metadata.name}') -c vault-agent
+kubectl -n vswh logs $(kubectl get pods -n vswh --selector=app.kubernetes.io/name=test-templating -o=jsonpath='{.items[0].metadata.name}') -c alpine
 #kubectl logs $(kubectl get pods --selector=app.kubernetes.io/name=test-templating -o=jsonpath='{.items[0].metadata.name}') -c vault-agent
 #kubectl logs $(kubectl get pods --selector=app.kubernetes.io/name=test-templating -o=jsonpath='{.items[0].metadata.name}') -c alpine
 kubectl wait pod -l app.kubernetes.io/name=test-templating --for=condition=ready --timeout=120s -A
 kubectl get pods -A
-test $(kubectl -n default exec -it $(kubectl get pods -n default --selector=app.kubernetes.io/name=test-templating -o=jsonpath='{.items[0].metadata.name}') -c alpine -- cat /vault/secrets/config.yaml | jq '.id' | xargs ) = "secretId"
+test $(kubectl -n vswh exec -it $(kubectl get pods -n vswh --selector=app.kubernetes.io/name=test-templating -o=jsonpath='{.items[0].metadata.name}') -c alpine -- cat /vault/secrets/config.yaml | jq '.id' | xargs ) = "secretId"
 #test $(kubectl exec -it $(kubectl get pods --selector=app.kubernetes.io/name=test-templating -o=jsonpath='{.items[0].metadata.name}') -c alpine -- cat /vault/secrets/config.yaml | jq '.id' | xargs ) = "secretId"
 
 kubectl apply -f deploy/test-deployment-seccontext.yaml
