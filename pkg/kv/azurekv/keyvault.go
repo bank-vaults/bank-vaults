ee3ea4233823ef58b8ffe77ef0752abbab2eea79
András Jáky <ajaky@cisco.com>
2023-08-11 11:11:33 +0200

    feat: implement a file provider for Azure auth (#2086)
    Signed-off-by: András Jáky <ajaky@cisco.com>

--- pkg/kv/azurekv/keyvault.go
+++ pkg/kv/azurekv/keyvault.go
@@ -15,25 +15,40 @@
 package azurekv
 
 import (
+	"bytes"
 	"context"
+	"encoding/binary"
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
+	"strings"
+	"unicode/utf16"
 
 	"emperror.dev/errors"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/policy"
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
 	"github.com/Azure/azure-sdk-for-go/sdk/security/keyvault/azsecrets"
-	"github.com/Azure/go-autorest/autorest/azure/auth"
+	"github.com/dimchansky/utfbom"
 
 	"github.com/bank-vaults/bank-vaults/pkg/kv"
 )
+
+const AzureAuthLocation = "AZURE_AUTH_LOCATION"
 
 // azureKeyVault is an implementation of the kv.Service interface, that encrypts
 // and decrypts and stores data using Azure Key Vault.
 type azureKeyVault struct {
 	client *azsecrets.Client
+}
+
+type AuthConfig struct {
+	TenantID     string `json:"tenantId"`
+	ClientID     string `json:"clientId"`
+	ClientSecret string `json:"clientSecret"`
 }
 
 var _ kv.Service = &azureKeyVault{}
@@ -44,19 +59,7 @@ func New(name string) (kv.Service, error) {
 		return nil, errors.Errorf("invalid Key Vault specified: '%s'", name)
 	}
 
-	// File based auth is not supported in the new SDK, hence this workaround
-	if _, ok := os.LookupEnv("AZURE_AUTH_LOCATION"); ok {
-		settings, err := auth.GetSettingsFromFile()
-		if err != nil {
-			log.Fatal(err)
-		}
-		os.Setenv(auth.TenantID, settings.Values[auth.TenantID])
-		os.Setenv(auth.ClientID, settings.Values[auth.ClientID])
-		os.Setenv(auth.ClientSecret, settings.Values[auth.ClientSecret])
-	}
-
-	// Create a credential using the NewDefaultAzureCredential type.
-	cred, err := azidentity.NewDefaultAzureCredential(nil)
+	cred, err := NewAzureAuthCredentials()
 	if err != nil {
 		log.Fatalf("failed to obtain a credential: %v", err)
 	}
@@ -92,4 +95,104 @@ func (a *azureKeyVault) Set(key string, val []byte) error {
 	parameters := azsecrets.SetSecretParameters{Value: &value}
 	_, err := a.client.SetSecret(context.Background(), key, parameters, nil)
 	return errors.Wrapf(err, "failed to set key: %s", key)
+}
+
+type AzureAuthCredentials struct {
+	creds map[string]azcore.TokenCredential
+}
+
+func (d *AzureAuthCredentials) GetToken(ctx context.Context, options policy.TokenRequestOptions) (azcore.AccessToken, error) {
+	var errorMessages []string
+
+	for name, cred := range d.creds {
+		if cred != nil {
+			token, err := cred.GetToken(ctx, options)
+			if err != nil {
+				errorMessages = append(errorMessages, name+": "+err.Error())
+			} else {
+				return token, nil
+			}
+		} else {
+			errorMessages = append(errorMessages, name+": nil")
+		}
+	}
+
+	return azcore.AccessToken{}, errors.Errorf("failed to obtain a token: %s", strings.Join(errorMessages, "\n"))
+}
+
+func NewAzureAuthCredentials() (*AzureAuthCredentials, error) {
+	var errorMessages []string
+	creds := make(map[string]azcore.TokenCredential)
+
+	defaultCred, err := azidentity.NewDefaultAzureCredential(nil)
+	if err != nil {
+		errorMessages = append(errorMessages, "DefaultCredential: "+err.Error())
+	}
+	creds["DefaultCredential"] = defaultCred
+
+	fileBasedCred, err := NewFileBasedCredential()
+	if err != nil {
+		errorMessages = append(errorMessages, "FileBasedCredential: "+err.Error())
+	}
+	creds["FileBasedCredential"] = fileBasedCred
+
+	if len(errorMessages) == 2 {
+		return nil, errors.Errorf("failed to obtain a credential: %v", strings.Join(errorMessages, "\n"))
+	}
+
+	return &AzureAuthCredentials{creds: creds}, nil
+}
+
+func NewFileBasedCredential() (azcore.TokenCredential, error) {
+	// Implementation based on github.com/Azure/go-autorest/autorest/azure/auth.GetSettingsFromFile()
+	fileLocation := os.Getenv(AzureAuthLocation)
+	if fileLocation == "" {
+		return nil, errors.Errorf("environment variable %s is not set", AzureAuthLocation)
+	}
+
+	contents, err := os.ReadFile(fileLocation)
+	if err != nil {
+		return nil, err
+	}
+
+	// Auth file might be encoded
+	decoded, err := decode(contents)
+	if err != nil {
+		return nil, err
+	}
+
+	var authFile AuthConfig
+	err = json.Unmarshal(decoded, &authFile)
+	if err != nil {
+		return nil, err
+	}
+
+	cred, err := azidentity.NewClientSecretCredential(authFile.TenantID, authFile.ClientID, authFile.ClientSecret, nil)
+	if err != nil {
+		return nil, err
+	}
+	return cred, nil
+}
+
+//nolint:exhaustive
+func decode(b []byte) ([]byte, error) {
+	reader, enc := utfbom.Skip(bytes.NewReader(b))
+
+	switch enc {
+	case utfbom.UTF16LittleEndian:
+		u16 := make([]uint16, (len(b)/2)-1)
+		err := binary.Read(reader, binary.LittleEndian, &u16)
+		if err != nil {
+			return nil, err
+		}
+		return []byte(string(utf16.Decode(u16))), nil
+	case utfbom.UTF16BigEndian:
+		u16 := make([]uint16, (len(b)/2)-1)
+		err := binary.Read(reader, binary.BigEndian, &u16)
+		if err != nil {
+			return nil, err
+		}
+		return []byte(string(utf16.Decode(u16))), nil
+	}
+	return io.ReadAll(reader)
 }
